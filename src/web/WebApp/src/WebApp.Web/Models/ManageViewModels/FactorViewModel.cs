e8844944e583ffc18ef361692d2582142e3afac9
Patryk Lotzwi <p.lotzwi@outlook.com>
2016-09-24 00:48:26 +0200

    Added Web app

---
+++ src/web/WebApp/src/WebApp.Web/Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Web.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
