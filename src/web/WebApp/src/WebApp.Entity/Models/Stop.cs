df087c83570a4deb6b82bbe600921d5f9f840ae0
Kamil Cieślak <kamil.cieslak@outlook.com>
2016-09-24 22:41:21 +0200

    PARTYABILITY!

---
+++ src/web/WebApp/src/WebApp.Entity/Models/Stop.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace WebApp.Entity.Models
+{
+    public class Stop
+    {
+        public int Id { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public StopType Type { get; set; }
+    }
+}
+
+[Flags]
+public enum StopType
+{
+    Unknown = 0,
+    Bus = 1,
+    Tram = 1 << 1
+}
