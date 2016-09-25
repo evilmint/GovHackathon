d8aaf6288ee664161a0f0094424872bc44b6af93
Patryk Lotzwi <p.lotzwi@outlook.com>
2016-09-25 03:51:31 +0200

    fixed libs

---
+++ src/web/WebApp/src/WebApp.Web/wwwroot/app/controller/mainController.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('mainController', mainController);
+
+    mainController.$inject = ['$location'];
+
+    function mainController($location, $scope, $http) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = 'mainController';
+
+        activate();
+
+        function activate() { }
+    }
+})();
