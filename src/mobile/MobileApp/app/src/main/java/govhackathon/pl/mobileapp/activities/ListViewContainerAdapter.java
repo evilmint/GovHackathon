20cefd5f5e858ffed1a593e20a4bce24e2639c0b
Janusz Bossy <janusz@bossy.io>
2016-09-25 07:42:10 +0200

    Adds partyability graph

--- src/mobile/MobileApp/app/src/main/java/govhackathon/pl/mobileapp/activities/ListViewContainerAdapter.java
+++ src/mobile/MobileApp/app/src/main/java/govhackathon/pl/mobileapp/activities/ListViewContainerAdapter.java
@@ -1,6 +1,7 @@
 package govhackathon.pl.mobileapp.activities;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,9 +9,25 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.AxisValueFormatter;
+
+import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import govhackathon.pl.mobileapp.R;
 
@@ -195,6 +212,56 @@ class ListViewContainerAdapter extends BaseAdapter {
                 }
                 case "partyability":
                     vi = inflater.inflate(R.layout.container_partyability, null);
+
+                    BarChart chart = (BarChart) vi.findViewById(R.id.chart);
+                    chart.setDrawBarShadow(false);
+                    chart.setDrawValueAboveBar(true);
+                    chart.setMaxVisibleValueCount(78);
+                    chart.setPinchZoom(false);
+                    chart.setDrawGridBackground(false);
+
+                    chart.setDescription("");
+
+                    chart.getLegend().setEnabled(false);
+
+                    YAxis yAxis = chart.getAxisLeft();
+                    yAxis.setAxisMaxValue(78f);
+                    yAxis.setAxisMinValue(0f);
+                    yAxis.setDrawLabels(false);
+                    yAxis.setDrawGridLines(false);
+                    yAxis.setDrawZeroLine(false);
+
+                    yAxis = chart.getAxisRight();
+                    yAxis.setAxisMaxValue(78f);
+                    yAxis.setAxisMinValue(0f);
+                    yAxis.setDrawLabels(false);
+                    yAxis.setDrawGridLines(false);
+                    yAxis.setDrawZeroLine(false);
+
+                    XAxis xAxis = chart.getXAxis();
+                    xAxis.setDrawLabels(false);
+                    xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+                    xAxis.setDrawGridLines(false);
+                    xAxis.setGranularity(.5f);
+                    xAxis.setLabelCount(3);
+                    xAxis.setDrawAxisLine(false);
+
+                    List<BarEntry> entries = new ArrayList<>();
+
+                    try {
+                        JSONArray values = containerObject.jsonObject.getJSONArray("partyability");
+                        for (int i = 0; i < values.length(); ++i) {
+                            JSONObject value = values.getJSONObject(i);
+                            entries.add(new BarEntry(i, (float)value.getDouble("value")));
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                    BarDataSet dataSet = new BarDataSet(entries, "LA Bell");
+                    dataSet.setColor(Color.parseColor("#53b256"));
+                    chart.setData(new BarData(dataSet));
+                    chart.invalidate();
                     break;
             }
         }
