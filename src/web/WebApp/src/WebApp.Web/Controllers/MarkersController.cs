9a7a0207e4f635e0a9501df2f365cdbaa20e68d1
Kamil Cieślak <kamil.cieslak@outlook.com>
2016-09-25 04:12:26 +0200

    Added new params for bounding box query

--- src/web/WebApp/src/WebApp.Web/Controllers/MarkersController.cs
+++ src/web/WebApp/src/WebApp.Web/Controllers/MarkersController.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using WebApp.Entity.Data;
-using WebApp.Web.Services;
 
 namespace WebApp.Web.Controllers
 {
@@ -15,17 +14,84 @@ namespace WebApp.Web.Controllers
             this.context = context;
         }
 
-        public IEnumerable<object> GetMarkersFromBoundingBox(double topRightLatitude, double topRightLongitude, double bottomLeftLatitude, double bottomLeftLongitude)
+        public IEnumerable<object> GetMarkersFromBoundingBox(double topRightLatitude, double topRightLongitude, double bottomLeftLatitude, double bottomLeftLongitude, string type = "", int limit = -1)
         {
-            var bb = new BoundingBox(topRightLatitude, topRightLongitude, bottomLeftLatitude, bottomLeftLongitude);
+            var result = new List<object>();
 
-            List<object> result = new List<object>();
-            var events = context.Events.Where(x => (x.Latitude < topRightLatitude && x.Longitude < topRightLongitude &&
-                x.Latitude > bottomLeftLatitude && x.Longitude > bottomLeftLongitude)).Take(100);
-            result.AddRange(events);
+            if (type == string.Empty || type == "events")
+            {
+                var events = context.Events
+                    .Where(x => x.Latitude < topRightLatitude
+                        && x.Longitude < topRightLongitude
+                        && x.Latitude > bottomLeftLatitude
+                        && x.Longitude > bottomLeftLongitude)
+                    .Take(limit >= 0 ? limit : 100)
+                    .ToList();
+                result.AddRange(events);
+            }
 
-            var middleSchools = context.Schools.Where(x => (x.Latitude < topRightLatitude && x.Longitude < topRightLongitude &&
-                x.Latitude > bottomLeftLatitude && x.Longitude > bottomLeftLongitude) && x.Typ == "00004").Take(10);
+            if (type == string.Empty || type == "preSchools")
+            {
+                var primarySchools = context.Schools
+                    .Where(x => x.Latitude < topRightLatitude
+                        && x.Longitude < topRightLongitude
+                        && x.Latitude > bottomLeftLatitude
+                        && x.Longitude > bottomLeftLongitude
+                        && x.Typ == "00001")
+                    .Take(limit >= 0 ? limit : 10)
+                    .ToList();
+                result.AddRange(primarySchools);
+            }
+
+            if (type == string.Empty || type == "primarySchools")
+            {
+                var primarySchools = context.Schools
+                    .Where(x => x.Latitude < topRightLatitude
+                        && x.Longitude < topRightLongitude
+                        && x.Latitude > bottomLeftLatitude
+                        && x.Longitude > bottomLeftLongitude
+                        && x.Typ == "00003")
+                    .Take(limit >= 0 ? limit : 10)
+                    .ToList();
+                result.AddRange(primarySchools);
+            }
+
+            if (type == string.Empty || type == "middleSchools")
+            {
+                var middleSchools = context.Schools
+                    .Where(x => x.Latitude < topRightLatitude
+                        && x.Longitude < topRightLongitude
+                        && x.Latitude > bottomLeftLatitude
+                        && x.Longitude > bottomLeftLongitude
+                        && x.Typ == "00004")
+                    .Take(limit >= 0 ? limit : 10)
+                    .ToList();
+                result.AddRange(middleSchools);
+            }
+
+            if (type == string.Empty || type == "relics")
+            {
+                var relics = context.Relics
+                    .Where(x => x.Latitude < topRightLatitude
+                        && x.Longitude < topRightLongitude
+                        && x.Latitude > bottomLeftLatitude
+                        && x.Longitude > bottomLeftLongitude)
+                    .Take(limit >= 0 ? limit : 10)
+                    .ToList();
+                result.AddRange(relics);
+            }
+
+            if (type == string.Empty || type == "stops")
+            {
+                var stops = context.Stops
+                    .Where(x => x.Latitude < topRightLatitude
+                        && x.Longitude < topRightLongitude
+                        && x.Latitude > bottomLeftLatitude
+                        && x.Longitude > bottomLeftLongitude)
+                    .Take(limit >= 0 ? limit : 20)
+                    .ToList();
+                result.AddRange(stops);
+            }
 
             return result;
         }
